3875c8af3f8c2b346058aeb0619a6f8c40a3401c
Vupi <276366791+FMD911@users.noreply.github.com>
2026-06-02 21:55:47 +0200

    ui polish: modern layout, cards, responsive design

--- src/state.js
+++ src/state.js
@@ -1,17 +1,4 @@
 import { createProject } from "./project.js";
-
-export const state = {
-  projects: [],
-  activeProjectId: null,
-};
-
-// create default project so app is never empty
-const defaultProject = createProject("Default");
-
-state.projects.push(defaultProject);
-state.activeProjectId = defaultProject.id;
-
-import { createProject } from "./project.js";
 import { loadFromStorage } from "./storage.js";
 
 const saved = loadFromStorage();
