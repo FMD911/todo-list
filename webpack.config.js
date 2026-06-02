57f98c02f967c9dbf28210834ae5d3094d502454
Vupi <276366791+FMD911@users.noreply.github.com>
2026-06-02 22:04:45 +0200

    fix: deploy build to docs for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
 
   entry: "./src/index.js",
 
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+output: {
+  filename: "main.js",
+  path: path.resolve(__dirname, "docs"),
+  clean: true,
+},
 
   devServer: {
     static: "./dist",
